dfc9d67bb5b56d93ad764bb5e598e104fe76bca6
KamilBozz <kbozkurt@my.bcit.ca>
2025-02-08 01:12:34 -0800

    add db

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,17 +3,18 @@ const mysql = require('mysql2/promise');
 const is_hosted = process.env.IS_HOSTED || false;
 
 const dbConfigHosted = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
+	host: "mysql-1f9e04c9-my-e658.g.aivencloud.com",
+	user: "avnadmin",
+	port: 14386,
+	password: "AVNS_5cAAyUss2odGFczuPcs",
+	database: "defaultdb",
 	multipleStatements: false
 };
 
 const dbConfigLocal = {
 	host: "localhost",
 	user: "root",
-	password: "Password",
+	password: "Yok-738738",
 	database: "database1",
 	multipleStatements: false
 };
